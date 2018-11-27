dd1dd55a8eb67f5d15d23735e3227be18ea6c3d8
xihe <citype124@gmail.com>
2018-11-27 18:08:23 +0800

    feature: 获取整个 blog 下所有文章的 url 以及其中 detail 信息

--- article-spider/ArticleSpider/ArticleSpider/spiders/jobbole.py
+++ article-spider/ArticleSpider/ArticleSpider/spiders/jobbole.py
@@ -1,39 +1,35 @@
 # -*- coding: utf-8 -*-
 import scrapy
 import re
+from scrapy.http import Request
+from urllib import parse
 
 class JobboleSpider(scrapy.Spider):
     name = 'jobbole'
     allowed_domains = ['blog.jobbole.com']
-    start_urls = ['http://blog.jobbole.com/110287/']
+    start_urls = ['http://blog.jobbole.com/all-posts/']
 
     def parse(self, response):
         """
-        调用 extract 方法
-        变成一个数组值，无法进一步 xpath
+        获取下一页的 url 并交给 scrapy 进行下载
+        1. 获取文章列表页中的文章 url 并交给解析函数进行具体字段的解析
+        2. 获取下一页的 url 并交给 scrapy 进行下载，下载完成后交给 parse 函数
         """
-        create_data = response.xpath('//p[@class="entry-meta-hide-on-mobile"]/text()').extract()[0].strip().replace("/",".")
+        post_urls = response.css("#archive .floated-thumb .post-thumb a::attr(href)")
+        for post_url in post_urls:
+            # Request(url=post_url,callback=self.parse_detail)
+            yield Request(url=parse.urljoin(response.url+post_url), callback=self.parse_detail)
+
+        # 下一页进行下载
+        next_url = response.css(".next.page-numbers::attr(href)").extract_first("")
+        if next_url:
+            yield Request(url=parse.urljoin(response.url,post_url), callback=self.parse_detail)
+
+    """
+    提取文章的具体字段
+    """
+    def parse_detail(self, response):
         title = response.xpath('//*[@id="post-110287"]/div[1]/h1/text()').extract()[0]
-        # 为什么会为空？ 包含某个 class 名称
-        vote = response.xpath("//span[@class='vote-post-up']/text()")
-        # 需要使用 contains 函数来调用 span 中包含的某种类名
-        vote = response.xpath("//span[contains(@class, 'vote-post-up')]/h10/text()").extract()[0]
-        fav_nums = response.xpath("//span[contains(@class,'bookmark-btn')]/text()").extract()[0]
-        match_re = re.match(".*？(\d+).*",fav_nums)
-        if match_re:
-            fav_nums = match_re.group(1)
-
-        comment_nums = response.xpath("//a[@href='#article-comment']/span").extract()
-        match_re = re.match(".*？(\d+).*",comment_nums)
-        if match_re:
-            comment_nums = match_re.group(1)
-        
-        content = response.xpath("//div[@class='entry']").extract()[0]
-
-        # 通过数组的方式 进行过滤
-        tag_list = response.xpath('//p[@class="entry-meta-hide-on-mobile"]/a/text()').extract()
-        [element for element in tag_list if not element.endswith("评论")]
-        tag = ','.join(tag_list)
         
         """
         使用 css 选择器
@@ -41,12 +37,18 @@ class JobboleSpider(scrapy.Spider):
         create_data = response.css("p.entry-meta-hide-on-mobile::text").extract()[0].strip().replace("/",".")
         praise_nums = response.css(".vote-post-up h10::text").extract()[0]
         fav_nums = response.css(".bookmark-btn::text").extract()[0]
+        match_re = re.match(".*？(\d+).*",fav_nums)
+        if match_re:
+            fav_nums = match_re.group(1)
+
         comment_nums = response.css("a[href='#article-comment'] span::text").extract()[0]
+        match_re = re.match(".*？(\d+).*",comment_nums)
+        if match_re:
+            comment_nums = match_re.group(1)
         content = response.css("div.entry").extract()[0]
         tags = response.css(".entry-meta-hide-on-mobile a::text").extract()[0]
+        [element for element in tags if not element.endswith("评论")]
         tags = ','.join(tags)
-
-
 
 
 

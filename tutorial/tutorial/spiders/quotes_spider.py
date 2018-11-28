46e91250ce76375fe9e9e81a74f2976da989aca2
xihe <citype124@gmail.com>
2018-11-28 11:00:37 +0800

    finish: 网络数据元素获取

---
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -0,0 +1,41 @@
+import scrapy
+
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
+    start_urls = [
+         'http://quotes.toscrape.com/page/1/',
+        'http://quotes.toscrape.com/page/2/',
+    ]
+
+    """
+    以下注释代码，其只要通过参数名 start_urls 就会默认实现
+    """
+    # def start_requests(self):
+    #     urls = [
+    #          'http://quotes.toscrape.com/page/1/',
+    #         'http://quotes.toscrape.com/page/2/',
+    #     ]
+    #     for url in urls:
+    #         yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        """
+        将两个网页的内容保存到本地 html 文件中
+        """
+        # page = response.url.split("/")[-2]
+        # filename = 'quotes-%s.html' % page
+        # with open(filename, 'wb') as f:
+        #     f.write(response.body)
+        # self.log("Save file %s" % filename)
+        for quote in response.css('div.quote'):
+            yield {
+                'text':quote.css('span.text::text').extract_first(),
+                'author':quote.css('small.author::text').extract_first(),
+                'tags':quote.css('div.tags a.tag::text').extract(),
+            }
+    
+    """
+    以上代码，只要调用命令
+    scrapy shell 'http://quotes.toscrape.com/page/1/'
+    就会产生默认结构。
+    """
